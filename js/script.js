fb22a1bd2e2f003f72c74f06c396d59b4d5974f2
Jessica Pena <jessica@scripted.org>
2018-12-07 19:27:17 +0000

    day 1 and part of day 2

--- js/script.js
+++ js/script.js
@@ -9,6 +9,45 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+        
+        var q1Answer = home(q1Result);
+        var q2Answer = breakfast(q2Result);
+        var q3Answer = superpower(q3Result);
+        
     });
 
 });
+
+function home(answer) {
+    if(answer === "tidy"){
+        return 0;
+    }
+    else if(answer === "messy"){
+        return 2;
+    }
+    else {
+        return 0;
+    }
+    
+}
+
+function breakfast(answer) {
+    if(answer === "pancakes"){
+        return 1;
+    }
+    else if(answer === "waffles"){
+        return 3;
+    }
+    else if(answer ==="french toast"){
+        return 5;
+    }
+    else {
+        return 0;
+    }
+    return result;
+}
+
+function superpower(answer) {
+    var result = 4;
+    return result;
+}
